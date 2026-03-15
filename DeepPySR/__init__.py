ffc99c229d129ed46ebb4b60358594698947a94e
FulingChen <chenfuling725@gmail.com>
2026-03-15 21:56:36 +0800

    initialize JuliaCall early and set environment variables to fix PySR segfaults and threading issues

--- DeepPySR/__init__.py
+++ DeepPySR/__init__.py
@@ -1,3 +1,16 @@
+import os
+
+# Fix for JuliaCall/PySR segfaults: Must be set before juliacall is imported
+os.environ["PYTHON_JULIACALL_HANDLE_SIGNALS"] = "yes"
+# Set Julia threads before initializing Julia
+os.environ["JULIA_NUM_THREADS"] = str(max(1, (os.cpu_count() or 2) - 1))
+
+# Try to initialize JuliaCall as early as possible to avoid conflict with torch
+try:
+    import juliacall
+except ImportError:
+    pass
+
 from .regressor import DeepPySRRegressor
 from .kan_regressor import KANPySRRegressor
 from .utils import mse_and_r2
