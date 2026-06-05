27d3903e4a422e03aca7c517d9b4c1704964dbf1
FulingChen <chenfuling725@gmail.com>
2026-06-05 12:35:52 +0800

    insulin

--- scripts/cv/run_diabetes_vps50_vpr100_aps10.0.sh
+++ scripts/cv/run_diabetes_vps50_vpr100_aps10.0.sh
@@ -13,13 +13,13 @@ export DEEPPYSR_PATH="/scratch/pawsey0411/fchen1/deeppysr.jl/python"
 export JULIA_DEPOT_PATH="/scratch/pawsey0411/fchen1/.julia_depot"
 export PYTHON_JULIAPKG_PROJECT="/scratch/pawsey0411/fchen1/DeepPySR/.venv/julia_env"
 export JULIA="$PYTHON_JULIAPKG_PROJECT/pyjuliapkg/install/bin/julia"
-$JULIA --project=$PYTHON_JULIAPKG_PROJECT -e 'import Pkg; Pkg.resolve(); Pkg.instantiate(); Pkg.precompile()'
-export PYTHON_JULIAPKG_OFFLINE=no
-
 cd $PROJECT_ROOT
 source ".venv/bin/activate"
 export PYTHONPATH="$PROJECT_ROOT:$PYTHONPATH"
 python -m juliapkg update
+export PYTHON_JULIAPKG_OFFLINE=no
+
+$JULIA --project=$PYTHON_JULIAPKG_PROJECT -e 'import Pkg; Pkg.resolve(); Pkg.instantiate(); Pkg.precompile()'
 set -e
 
 echo "Starting diabetes_vps50_vpr100_aps10.0 at $(date)"
