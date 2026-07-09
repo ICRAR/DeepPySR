23c4c6c5151a165bb046f4de9063a6485b517f6f
FulingChen <chenfuling725@gmail.com>
2026-07-09 14:18:32 +0800

    clean the repo

--- build/lib/DeepPySR/__init__.py
+++
@@ -1,6 +0,0 @@
-from .regressor import DeepPySRRegressor
-from .kan_regressor import KANPySRRegressor
-from .utils import mse_and_r2
-
-__version__ = "0.1.0"
-__all__ = ["DeepPySRRegressor", "KANPySRRegressor", "mse_and_r2"]
