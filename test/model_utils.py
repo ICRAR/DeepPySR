c38bd3dec496ddaca24f491a0c9f820bd4d8e278
FulingChen <chenfuling725@gmail.com>
2026-08-30 13:11:33 +0800

    Remove aggregated results CSVs for `bodyfat` and `heart` baselines and tests.

--- test/model_utils.py
+++ test/model_utils.py
@@ -472,7 +472,7 @@ def get_pysr_base_kwargs(os_cpu_count=None, use_explicit_cond=False, save_hof=Fa
         # "warm_start": True,
         "unary_operators": ["exp", "log", "sin", "sqrt"],
         "parsimony": 0.001,
-        "populations": 100,
+        "populations": 150,
         "population_size": 200,
         "ncycles_per_iteration": 200,
         "verbosity": 0,
